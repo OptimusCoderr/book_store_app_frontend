85dd0c956f54e250b0022b2f29faf3153a48d401
OptimusCoderr <chukwuebukaanulunko@gmail.com>
2024-11-30 14:55:42 +0100

    Updated baseUrl.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000"
+    return "https://book-store-app-backend-iota.vercel.app"
 }
 
 export  default getBaseUrl;  // Export the function to be used elsewhere
